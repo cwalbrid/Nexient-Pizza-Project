6371f4ccf3cc85706c54db56a0cb552ffa46cae7
Colin Walbridge <cwalbrid@umich.edu>
2017-03-01 13:15:35 -0500

    all copy blurbs added

--- js/factory.js
+++ js/factory.js
@@ -20,8 +20,9 @@ app.factory('viewStore', function(){
 			data: {
 				franchise: 'Empire State',
 				city: 'New York',
-				blurb: 'Empire State is the original Brick Oven Pizza Co franchise. Our founder, Giuseppe Giacosa, believed the best pizza came from top notch ingredients and a little Sicilian know-how. Millions of satisfied customers later, his legacy lives on.',
+				blurb: 'Empire State is the original Brick Oven Pizza Company. Giuseppe Giacosa dreamt of a neighborhood pizzeria where ordinary folks could gather to share delicious food and enjoy one another’s company. As word spread about the restaurant, customers came from miles around to experience the Best Pizza in the Borough.',
 				date: '1952',
+				phone:'(718) 599-2211',
 				myStyle: {
 						'background-color' : '#42423D',
 						'background' : "linear-gradient(to right, rgba(106, 106, 98, .3), rgba(106, 106, 98, .3)), url('../img/empire-state.jpg')",
@@ -34,17 +35,17 @@ app.factory('viewStore', function(){
 				pizzaData: [
 					{
 						name: 'Four Cheese Classic',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-						ingredients: 'mozzarella, asiago, provalone, parmesan'
+						blurb: 'A signature blend of cheeses that will make your mouth water',
+						ingredients: 'mozzarella, asiago, feta, parmesan'
 					},
 					{
 						name: 'Margherita Fresca',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A lighter, health-conscious pie packed with fresh flavorsA',
 						ingredients: 'mushrooms, tomatoes, basil, olive oil'
 					},
 					{
 						name: 'Patrick' + "'" + 's Favorite Pepperoni',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A unique pepperoni recipe invented by Giuseppe’s best friend',
 						ingredients: 'pepperoni, onion, garlic, red bell pepper'
 					}
 				]
@@ -55,8 +56,9 @@ app.factory('viewStore', function(){
 			data: {
 				franchise: 'Windy City',
 				city: 'Chicago',
-				blurb: 'Empire State is the original Brick Oven Pizza Co franchise. Our founder, Giuseppe Giacosa, believed the best pizza came from top notch ingredients and a little Sicilian know-how. Millions of satisfied customers later, his legacy lives on.',
+				blurb: 'Rumor has it Giuseppe founded the Windy City Brick Oven Pizza franchise when his High School Sweetheart, Loretta Simmons, moved to Chicago to pursue a career as a blues singer. Chicagoans fell in love with Brick Oven Pizza’s signature style, and Giuseppe married Loretta in 1964.',
 				date: '1963',
+				phone:'(773) 525-4582',
 				myStyle: {
 						'background-color' : '#42423D',
 						'background' : "linear-gradient(to right, rgba(106, 106, 98, .3), rgba(106, 106, 98, .3)), url('../img/windy-city.jpg')",
@@ -69,17 +71,17 @@ app.factory('viewStore', function(){
 				pizzaData: [
 					{
 						name: 'Goat Cheese Special',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A distinct goat-cheese pizza made with Wisconsin Chevre',
 						ingredients: 'mozzarella, chevre, chives'
 					},
 					{
 						name: 'Super Mario Combo',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A veggie pie inspired by Giuseppe’s cousin, Mario, who loved to eat olives from the can',
 						ingredients: 'mushrooms, black olives, green olives, banana peppers, onion'
 					},
 					{
 						name: 'All The Meats',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A hearty classic for the discerning carnivore',
 						ingredients: 'pepperoni, sausage, ham, bacon'
 					}
 				]
@@ -90,8 +92,9 @@ app.factory('viewStore', function(){
 			data: {
 				franchise: 'Golden Gate',
 				city: 'San Francisco',
-				blurb: 'Empire State is the original Brick Oven Pizza Co franchise. Our founder, Giuseppe Giacosa, believed the best pizza came from top notch ingredients and a little Sicilian know-how. Millions of satisfied customers later, his legacy lives on.',
-				date: '1969',
+				blurb: 'Giuseppe and Loretta Giacosa yearned to see the West Coast, and established the Golden Gate Brick Oven Pizza franchise during San Francisco’s Summer of Love. Mobs of local Flower Children swarmed the pizzeria, and it became a favorite spot for local musicians, poets, and artists to exhibit their work.',
+				date: '1967',
+				phone:'(415) 668-5587',
 				myStyle: {
 						'background-color' : '#42423D',
 						'background' : "linear-gradient(to right, rgba(106, 106, 98, .3), rgba(106, 106, 98, .3)), url('../img/golden-gate.jpg')",
@@ -104,17 +107,17 @@ app.factory('viewStore', function(){
 				pizzaData: [
 					{
 						name: 'Cow Tipper',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-						ingredients: 'mozzarella, feta, monterey jack'
+						blurb: 'An all-cheese favorite crafted with locally-sourced ingredients',
+						ingredients: 'mozzarella, provalone, monterey jack'
 					},
 					{
 						name: 'Califonia Sunrise',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A bright, slightly acidic veggie pie that is sure to satisfy',
 						ingredients: 'green bell peppers, sun-dried tomatoes, mushrooms, spinach'
 					},
 					{
 						name: 'Hawaiian Barbecue',
-						blurb: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+						blurb: 'A sweet and savory Hawaiian recipe with a tangy kick',
 						ingredients: 'ham, pineapple, pepperoni, bbq sauce'
 					}
 				]
